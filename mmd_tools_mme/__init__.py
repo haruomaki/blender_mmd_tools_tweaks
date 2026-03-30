1850f584fd7dbe99353a829c12c81d97220f90a4
Haruo <haruo-mtok@outlook.com>
2026-03-31 00:50:06 +0900

    MMEエフェクトを出力する機能

--- mmd_tools_mme/__init__.py
+++ mmd_tools_mme/__init__.py
@@ -6,20 +6,44 @@ import builtins
 import bpy
 from . import properties, panel
 
-# from . import tw
+
+def get_mmd_root() -> bpy.types.Object | None:
+    """選択したオブジェクトが含まれるMMD物体のルートを返す"""
+    obj = bpy.context.active_object
+    while obj != None:
+        if obj.mmd_type == "ROOT":
+            return obj
+        obj = obj.parent
+    return None
+
+
+def traverse_materials():
+    root = get_mmd_root()
+    arm = bpy.data.objects[f"{root.name}_arm"]
+    mats = set()
+    for obj in arm.children:
+        for slot in obj.material_slots:
+            m = slot.material
+            if m not in mats:
+                mats.add(m)
+                yield m
+
+
+def dump_effects(n=None):
+    char = "◯" if n is None else str(n)
+    for i, m in enumerate(traverse_materials()):
+        print(f"Pmd{char}[{i}] = {m.mme.effect_path}")
 
 
 def register():
-    # インタラクティブコンソールの名前空間に追加
-    # print("registerです")
     properties.register()
     panel.register()
-    # builtins.tw = tw
+    builtins.mme = type("mme", (), {})()
+    builtins.mme.trav = traverse_materials
+    builtins.mme.dump = dump_effects
 
 
 def unregister():
-    # print("unregisterです")
+    del builtins.mme
     panel.unregister()
     properties.unregister()
-    # if hasattr(builtins, "tw"):
-    # del builtins.tw
